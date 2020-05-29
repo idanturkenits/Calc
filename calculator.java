9bd48ceef51830004870dda726ee66a5164334c1
idanturkenits <60852129+idanturkenits@users.noreply.github.com>
2020-05-29 13:10:33 +0300

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -1,27 +1,31 @@
-
-import java.text.DecimalFormat;
 import java.util.*;
-
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
 import java.math.*;
-public class Calc {
+public class Eval {
 
 	
 	public static Scanner in = new Scanner(System.in);
 	
-	public static void main(String [] args)throws ScriptException{
+	public static void main(String [] args){
 		
 		char operation = regularSing();
-	
-		System.out.println();
+		int n1 = (int)(Math.random()*19+1);
+		int n2 = (int)(Math.random()*19+1);
 		
+		while(operation!= '!'){
+			play(n1 , n2 , operation);
+		
+			 n1 = (int)(Math.random()*19+1);
+			 n2 = (int)(Math.random()*19+1);
+			 operation = regularSing();
+		}
 	}
 	
 	
+	
 	public static char regularSing(){
+		System.out.println("Enter the operation");
 		char ch = in.next().charAt(0);
-		while(ch!='+' && ch!='*' && ch!='/' && ch!='-' && ch!='%'){
+		while(ch!='+' && ch!='*' && ch!='/' && ch!='-' && ch!='%' && ch!= '!'){
 			System.out.println("You stupid! this aint operation");
 			ch = in.next().charAt(0);
 		}
@@ -30,14 +34,40 @@ public class Calc {
 	}
 	
 	
-	public static void printTargil(int x , int y , char c)throws ScriptException{
+	public static void printTargil(int x , int y , char c){
 		System.out.println(x + " " + c + " "+ y + " = ? ");
 	}
 	
-	public static String solveTargil(String targil) throws ScriptException{
-		System.out.println((String)new ScriptEngineManager().getEngineByName("js").eval(targil));
-		return new DecimalFormat("#").format(Math.floor(Double.parseDouble(new ScriptEngineManager().getEngineByName("js").eval("9/4").toString())));
+	public static int solveTargil(int x , int y , char c){
+		switch(c){
+			case '+' : return x + y; 
+			case '-' : return x - y;
+			case '/' : return x / y;
+			case '*' : return x * y;
+			case '%' : return x % y;
+			
+		}
+		return -1;
+			
 	}
 	
-	
+	public static void play(int n1 , int n2 , char operation){
+		
+		
+		
+		printTargil(n1,n2,operation);
+		int answer = solveTargil(n1,n2,operation);
+		
+		for(int i = 3; i > 0 ; i--){
+			System.out.println("Enter your answer : ");
+			int userAnswer = in.nextInt();
+			if(userAnswer == answer){
+				System.out.println("You Are Correct!!");
+				System.out.println("You finished in " + (4-i) + " tries. Good Job!");
+				return;
+			}else{
+				System.out.println("Your are Wrong , you have " + (i-1) + " tries left");
+			}
+		}
+	}
 }
